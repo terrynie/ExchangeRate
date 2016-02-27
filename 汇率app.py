065b17598f3ba8ed7332936e8e1f30a3cf51ea5e
terry408911 <519046658@qq.com>
2016-02-27 23:25:41 +0800

    insert date into database

--- 汇率app.py
+++ 汇率app.py
@@ -1,22 +1,13 @@
 #coding:utf-8
 import urllib2
-# respose = urllib2.urlopen('http://forex.hexun.com/rmbhl/')
-# html = respose.read()
-# print(html)
+import re
+import MySQLdb
 
-# import urllib.request
-
-# url = 'http://forex.hexun.com/rmbhl/#zkRate'
-# req = urllib.request.Request(url, headers = {
-#     'Connection': 'Keep-Alive',
-#     'Accept': 'text/html, application/xhtml+xml, */*',
-#     'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko'
-# })
-# oper = urllib.request.urlopen(req)
-# data = oper.read()
-# print(data.decode())
-
+def strToFloat(str):
+    mFloat = 0.0
+    if(str != ''):
+        mFloat = float(str)
+    return mFloat
 
 request = urllib2.Request('http://data.bank.hexun.com/other/cms/foreignexchangejson.ashx?callback=ShowDatalist',data=None,headers = {
     'Connection': 'Keep-Alive',
@@ -25,8 +16,27 @@ request = urllib2.Request('http://data.bank.hexun.com/other/cms/foreignexchangej
     'Cookie':'vjuids=-33daed2da.152fe7ce078.0.41c5eb1c; vjlast=1455968543.1456497069.13; HexunTrack=SID=2016022019423407476fdfcb307fe4b3abfa46be4d178e670&CITY=41&TOWN=410100',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
 })
-# request.add_header('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36')
+
 response = urllib2.urlopen(request)
 html = response.read()
 html = html.decode('gb2312','ignore').encode('utf-8')
-print(html)
+contents = re.findall("{bank:'(.*?)',currency:'(.*?)',code:'(.*?)',currencyUnit:'(.*?)',cenPrice:'(.*?)',buyPrice1:'(.*?)',sellPrice1:'(.*?)',buyPrice2:'(.*?)',sellPrice2:'(.*?)',releasedate:'(.*?)'}",html,re.S)
+# connect to MySQL
+conn=MySQLdb.connect(host="localhost",user="root",passwd="root",db="exchange",charset="utf8")
+cursor = conn.cursor()
+
+for content in contents:
+    if(content[1] != ''):
+        bank = content[0]
+        currency = content[1]
+        code = content[2]
+        currencyUnit = content[3]
+        cenPrice = strToFloat(content[4])
+        remittanceBuyPrice = strToFloat(content[5])
+        sellPrice = strToFloat(content[6])
+        cashBuyPrice = strToFloat(content[7])
+        sellPrice2 = strToFloat(content[8])
+        releasedate = content[9]
+        sql = "INSERT INTO exchange_rate(bank, currency, code, currencyUnit, cenPrice, remittanceBuyPrice, sellPrice, cashBuyPrice, sellPrice2, releasedate) VALUES('%s','%s','%s','%s','%f','%f','%f','%f','%f','%s') " % (bank,currency,code,currencyUnit,cenPrice,remittanceBuyPrice,sellPrice,cashBuyPrice,sellPrice2,releasedate)
+        cursor.execute(sql)
+        conn.commit()
